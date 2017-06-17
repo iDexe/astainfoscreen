17cd6646f684d0392c5ccdcc25c827cf25db9506
iDexe <nils@Nilss-Mac-mini.local>
2017-06-17 23:44:10 +0200

    minor changes

--- node.lua
+++ node.lua
@@ -9,28 +9,23 @@ local font = resource.load_font("OpenSans-Regular.ttf")
 
 
 function wrap(str, limit, indent, indent1)
-    limit = limit or 72
-    local here = 1
-    local wrapped = str:gsub("(%s+)()(%S+)()", function(sp, st, word, fi)
-        if fi-here > limit then
-            here = st
-            return "\n"..word
-        end
-    end)
-    local splitted = {}
-    for token in string.gmatch(wrapped, "[^\n]+") do
-        splitted[#splitted + 1] = token
-    end
-    return splitted
+  indent = indent or ""
+  indent1 = indent1 or indent
+  limit = limit or 72
+  local here = 1-#indent1
+  return indent1..str:gsub("(%s+)()(%S+)()",
+                          function(sp, st, word, fi)
+                            if fi-here > limit then
+                              here = st - #indent
+                              return "\n"..indent..word
+                            end
+                          end)
 end
-
-
 
 
 util.file_watch("NextdayMeals.json", function(content)
 	meals = json.decode(content)
 	for idx, meal in ipairs(meals) do
-		meal.lines = wrap(meal.name, 45)
 
 
 	end
@@ -39,11 +34,9 @@ end)
 
 function node.render()
     gl.clear(0,0,0,1)
-    font:write(20, 20, "Mensa\nplan", 128, 1,1,1,1)
+    font:write(20, 20, "Mensaplan", 128, 1,1,1,1)
     for idx, meal in ipairs(meals) do
     	font:write(20, 80 * (1+idx), meal.name, 32, 1,1,1,1)
-    	for idx2, line in ipairs(meal) do
-    		font:write(40, 80 * (1+idx) + idx2 * 32, line, 32, 1,1,1,1)
-    	end
+    	font:write(20, 80 * (1+idx) + 40, wrap(meal.name, 35), 32, 1,1,1,1)
     end
 end
