e164bae13c9b15b5520883d2b262a98aa90ae872
iDexe <nils@Nilss-Mac-mini.local>
2017-06-17 15:25:00 +0200

    initial commit

---
+++ node.lua
@@ -0,0 +1,11 @@
+gl.setup(1920, 1080)
+
+util.resource_loader{
+    "OpenSans-Regular.ttf"
+}
+local font = resource.load_font("OpenSans-Regular.ttf")
+
+function node.render()
+    gl.clear(0,0,0,1)
+    font:write(250, 300, "Communism will win!", 128, 1,1,1,1)
+end
