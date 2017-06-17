8efd01efd50eb759ca32447ab330322967c6657a
iDexe <nils@Nilss-Mac-mini.local>
2017-06-17 22:34:15 +0200

    list test

--- node.lua
+++ node.lua
@@ -7,15 +7,20 @@ util.resource_loader{
 }
 local font = resource.load_font("OpenSans-Regular.ttf")
 
-function node.render()
-    gl.clear(0,0,0,1)
-    font:write(250, 300, "Test", 128, 1,1,1,1)
-end
 
 util.file_watch("NextdayMeals.json", function(content)
 	meals = json.decode(content)
 	for idx, meal in ipairs(meals) do
-		
+
 
 	end
 end)
+
+
+function node.render()
+    gl.clear(0,0,0,1)
+    font:write(20, 20, "Mensaplan", 64, 1,1,1,1)
+    for idx, meal in ipairs(meals) do
+    	font:write(20, 64 * (1+idx), meal.name, 64, 1,1,1,1)
+    end
+end
