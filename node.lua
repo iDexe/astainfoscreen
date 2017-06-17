f8a015b43fc715d4a8d292298b19fa285c6e6b06
iDexe <nils@Nilss-Mac-mini.local>
2017-06-18 00:19:41 +0200

    minor changes

--- node.lua
+++ node.lua
@@ -38,7 +38,7 @@ function node.render()
     font:write(20, 20, "Mensaplan", 128, 1,1,1,1)
     for idx, meal in ipairs(meals) do
     	font:write(20, 96 * (1+idx), meal.name, 32, 1,1,1,1)
-    	for line in meal.name:gmatch("[^\r\n]+") do
+    	for index, line in meal.name:gmatch("[^\r\n]+") do
     		font:write(20, 96 * (1+idx) + 32 , line, 32, 1,1,1,1)
     	end
     end
